2300d732c5bfe6c70e3f79cd9275ffe25cc7cb5c
batflyer <alexander@batflyer.net>
2018-02-13 14:03:59 -0600

    Added the BSD 2-Clause license, added the license blurb to the top of the source file, updated the docstring for inferModes.py, and added authorship information.

--- inferModes.py
+++ inferModes.py
@@ -1,14 +1,45 @@
-"""
-Alexander L. Hayes (@batflyer)
+"""inferModes.py: A Python package for inferring modes from a dataset.
+                  Possibly for converting documents to a minimized form.
 
-Notes:
-Currently a proof-of-concept for converting pos, neg, and
-facts to a minimized format.
-"""
+BSD 2-Clause License
+
+Copyright (c) 2018, Alexander L. Hayes
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
 
 from __future__ import print_function
 from collections import Counter
 import argparse
+
+__author__ = "Alexander L. Hayes (@batflyer)"
+__copyright__ = "Copyright 2018, Alexander L. Hayes"
+__credits__ = ["Alexander L. Hayes", "Kaushik Roy", "Sriraam Natarajan"]
+
+__license__ = "BSD 2-Clause"
+__version__ = "0.0.1"
+__maintainer__ = "Alexander L. Hayes (@batflyer)"
+__email__ = "alexander@batflyer.net"
+__status__ = "Prototype"
 
 class SetupArguments:
     """
