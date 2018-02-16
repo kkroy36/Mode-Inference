e89735844a55ecbedb858c65f3682527b13e97d1
batflyer <alexander@batflyer.net>
2018-02-16 17:39:22 -0600

    Removing builtins import since Python 2 should not need it.

--- inferModes.py
+++ inferModes.py
@@ -28,7 +28,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
 
 from __future__ import print_function
-from builtins import object
 from collections import Counter
 import argparse
 import re
